f15e9edc8f72ee7be460de7cf9f3d231d16ccf94
divyamehta191 <divyamehta610@gmail.com>
2026-05-11 16:45:06 +0530

    push origin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function GalleryLayout(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: "pink",padding: "20px" }}>
+    <div style={{ marginBottom: "20px"}} >
+        {props.even}
+      </div>
+
+  
+      <div style={{marginBottom: "20px" }}>
+        {props.odd}
+      </div>
+
+      <div>
+        {props.children}
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default function App() {
+
+  return (
+
+    <GalleryLayout even={
+        <ImageGroup
+          images={[
+            "images.png",
+              "images (1).png",
+              "images (2).png",
+          "images (3).png",
+              "images (1).png",
+          ]}
+        />
+      }
+
+      odd={
+        <ImageGroup
+          images={[
+            
+            "images (5).png",
+            "download.png",
+              "download (1).png",
+          ]}
+        />
+      }
+
+    >
+
+      <ImageGroup
+        images={[
+          "download (2).png",
+          "download (3).png",
+          "download (1).png",
+          "images.png",
+        ]}
+      />
+
+    </GalleryLayout>
+
+  );
+}
+
+function ImageGroup({ images }) {
+
+  const isOdd = images.length % 2 !== 0;
+
+  return (
+
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "30px",
+        justifyContent:"center"
+      }}
+    >
+
+      {images.map((img, index) => {
+
+        const isLastImage =
+          isOdd && index === images.length - 1;
+
+        return (
+          <img
+            key={index}
+            src={img}
+            alt=""
+            style={{
+              width: isLastImage ? "100%" : "49%",
+               height: isLastImage ? "400px" : "350px",
+              objectFit: "cover"
+            }}
+          />
+        );
+      })}
+
+    </div>
+
+  );
+}
